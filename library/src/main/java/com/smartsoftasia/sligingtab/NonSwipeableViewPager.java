44acfea4dbaccc046de620a602530555436cda92
gregoire <greg@smartsoftasia.com>
2015-04-21 15:38:01 +0700

    init commit

---
+++ library/src/main/java/com/smartsoftasia/sligingtab/NonSwipeableViewPager.java
@@ -0,0 +1,34 @@
+package com.smartsoftasia.sligingtab;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by gregoire barret on 3/19/15.
+ * For neibo-android project.
+ */
+public class NonSwipeableViewPager extends ViewPager {
+    public static final String TAG = "NonSwipeableViewPager";
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+}
